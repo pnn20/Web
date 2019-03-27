3e9c36be174a4c4b56242f316d020f860b74919a
DESKTOP-HINL2IQ\cshacker <dohyun_kim@tmax.co.kr>
2019-03-27 09:49:51 +0900

    send params in router

--- NodeJS/Example/WebServer/express_router.js
+++ NodeJS/Example/WebServer/express_router.js
@@ -7,12 +7,14 @@ app.use(bodyParser.json());
 app.use('/public', static(path.join(__dirname, 'public')));
 
 var router = express.Router();
-router.route('/process/login').post(function(req, res) {
+router.route('/process/login/:name').post(function(req, res) {
     console.log('/process/login logic');
+    var paramName = req.params.name;
     var paramId = req.body.id || req.query.id;
     var paramPassword = req.body.password || req.query.password;
 
     res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+    res.write('<div><p>Param name : ' + paramName + '</p></div>');
     res.write('<div><p>Param id : ' + paramId + '</p></div>');
     res.write('<div><p>Param password : ' + paramPassword + '</p></div>');
     res.write("<br><br><a href='/public/login2.html'> back to login page </a>");
@@ -24,4 +26,4 @@ app.use('/', router);
 app.set('port', process.env.PORT || 3000);
 http.createServer(app).listen(app.get('port'), function() {
     console.log('express server started : ' + app.get('port'));
-});
+}); // 172
